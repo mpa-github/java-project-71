95963e5c920c241bf9531cd5e8def3694210f4c9
Pavel Muravyev <muravyev.pavel@gmail.com>
2023-02-03 12:30:14 +0300

    Made App Class final.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,7 +9,7 @@ import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
          description = "Compares two configuration files and shows a difference.")
-final public class App implements Callable<Integer> {
+final class App implements Callable<Integer> {
 
     @Option(names = {"-f", "--format"},
             defaultValue = "stylish",
